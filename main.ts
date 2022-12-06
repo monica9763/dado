a1dc4d5eb9d8c45a93ffb4b7689ca1c4febe72d3
monica9763 <119902292+monica9763@users.noreply.github.com>
2022-12-06 22:32:49 +0100

    File iniziali per il progetto MakeCode

---
+++ main.ts
@@ -0,0 +1,58 @@
+let numero = 0
+input.onGesture(Gesture.Shake, function () {
+    numero = randint(1, 6)
+    if (numero == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (numero == 2) {
+        basic.showLeds(`
+            . . . . .
+            . # . . .
+            . . . . .
+            . . . # .
+            . . . . .
+            `)
+    }
+    if (numero == 3) {
+        basic.showLeds(`
+            . . . . .
+            . # . . .
+            . . # . .
+            . . . # .
+            . . . . .
+            `)
+    }
+    if (numero == 4) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            `)
+    }
+    if (numero == 5) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . # . .
+            . # . # .
+            . . . . .
+            `)
+    }
+    if (numero == 6) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . # . # .
+            . # . # .
+            . . . . .
+            `)
+    }
+})
